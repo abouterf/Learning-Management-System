18a6dca7994640bd72023729fb3fc2bfc14d57ea
Erfan kargosha <ekargosha88@gmail.com>
2020-10-17 04:52:32 +0330

    start with blade component

---
+++ app/View/Components/ValidationError.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class ValidationError extends Component
+{
+    public $field;
+    public function __construct($field)
+    {
+        $this->field = $field;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        return view('components.validation-error');
+    }
+}
--- modules/abouterf/Course/Resources/Views/create.blade.php
+++ modules/abouterf/Course/Resources/Views/create.blade.php
@@ -1,73 +1,73 @@
 @extends('Dashboard::master')
-@section('content')
 @section('breadcrumb')
-<li><a href="{{route('courses.index')}}" title="دوره ها">دوره ها</a></li>
-<li><a href="#" title="ویرایش دوره">ویرایش دوره
-    </a></li>
+    <li><a href="{{ route('courses.index') }}" title="دوره ها">دوره ها</a></li>
+    <li><a href="#" title="ویرایش دوره">ویرایش دوره</a></li>
 @endsection
-<div class="row no-gutters  ">
+@section('content')
+    <div class="row no-gutters  ">
+        <div class="col-12 bg-white">
+            <p class="box__title">بروزرسانی دوره</p>
+            <form action="{{ route('courses.store') }}" class="padding-30" method="post">
+                @csrf
+                <x-input name="title" placeholder="عنوان دوره" type="text" required/>
+                <x-input type="text" name="slug" placeholder="نام انگلیسی دوره" class="text-left" required />
 
-    <div class="col-12 bg-white">
-        <p class="box__title">ایجاد دوره</p>
 
-        <form action="{{route('courses.store')}}" method="post" class="padding-30">
-            @csrf
-            <input type="text" class="text" name="title" placeholder="عنوان دوره" required>
-            <input type="text" class="text text-left " name="slug" placeholder="نام انگلیسی دوره" required>
-
-            <div class="d-flex multi-text">
-                <input type="text" class="text text-left mlg-15" name="priority" placeholder="ردیف دوره">
-                <input type="text" placeholder="مبلغ دوره" name="price" class="text-left text mlg-15" required>
-                <input type="number" placeholder="درصد مدرس" name="percent" class="text-left text" required>
-            </div>
-            <select name="teacher_id" required>
-                <option value="">انتخاب مدرس دوره</option>
-                @foreach($teachers as $teacher)
-                <option value="{{$teacher->id}}">{{$teacher->name}}</option>
-                @endforeach
-            </select>
-
-            <ul class="tags">
-                <li class="tagAdd taglist">
-                    <input type="text" name="type" id="search-field" placeholder="برچسب ها">
-                </li>
-            </ul>
-            <select name="type" required>
-                <option value="">نوع دوره</option>
-                @foreach (\abouterf\Course\Models\Course::$types as $item)
-                <option value="{{$item}}">@lang($item)</option>
-                @endforeach
-            </select>
-            <select name="status" required>
-                <option value="">وضعیت دوره</option>
-                @foreach (\abouterf\Course\Models\Course::$statuses as $item)
-                <option value="{{$item}}">@lang($item)</option>
-                @endforeach
-                
-            </select>
-            <select name="category_id" required>
-                <option value="">دسته بندی </option>
-                @foreach($categories as $category)
-                <option value="{{$category->id}}">{{$category->title}} </option>
-                @endforeach
-            </select>
-
-            <div class="file-upload">
-                <div class="i-file-upload">
-                    <span>آپلود بنر دوره</span>
-                    <input type="file" class="file-upload" id="files" name="image" required />
+                <div class="d-flex multi-text">
+                    <x-input type="text" class="text-left mlg-15" name="priority" placeholder="ردیف دوره" />
+                    <x-input type="text" placeholder="مبلغ دوره" name="price" class="text-left" required />
+                    <x-input type="number" placeholder="درصد مدرس" name="percent" class="text-left" required />
                 </div>
-                <span class="filesize"></span>
-                <span class="selectedFiles">فایلی انتخاب نشده است</span>
-            </div>
-            <textarea placeholder="توضیحات دوره" name="body" class="text h"></textarea>
-
-            <button class="btn btn-webamooz_net">ایجاد</button>
-        </form>
+                <select name="teacher_id" required>
+                    <option value="">انتخاب مدرس دوره</option>
+                    @foreach($teachers as $teacher)
+                    <option value="{{ $teacher->id }}">{{ $teacher->name }}</option>
+                    @endforeach
+                </select>
+                <x-validation-error field="teacher_id"/>
+                <ul class="tags">
+                    <li class="tagAdd taglist">
+                        <input type="text" name="tags" id="search-field" placeholder="برچسب ها">
+                        <x-validation-error field="tags"/>
+                    </li>
+                </ul>
+                <select name="type" required>
+                    <option value="">نوع دوره</option>
+                    @foreach(\abouterf\Course\Models\Course::$types as $type)
+                        <option value="{{ $type }}">@lang($type)</option>
+                    @endforeach
+                </select>
+                <x-validation-error field="type"/>
+                <select name="status" required>
+                    <option value="">وضعیت دوره</option>
+                    @foreach(\abouterf\Course\Models\Course::$statuses as $status)
+                        <option value="{{ $status }}">@lang($status)</option>
+                    @endforeach
+                </select>
+                <select name="category_id" required>
+                    <option value="">دسته بندی</option>
+                    @foreach($categories  as $category)
+                        <option value="{{ $category->id }}">{{ $category->title }}</option>
+                    @endforeach
+                </select>
+                <x-validation-error field="category_id"/>
+                <div class="file-upload">
+                    <div class="i-file-upload">
+                        <span>آپلود بنر دوره</span>
+                        <input type="file" class="file-upload" id="files" name="image" required/>
+                    </div>
+                    <span class="filesize"></span>
+                    <span class="selectedFiles">فایلی انتخاب نشده است</span>
+                    <x-validation-error field="image"/>
+                </div>
+                <textarea placeholder="توضیحات دوره" name="body" class="text h"></textarea>
+                <x-validation-error field="body"/>
+                <button class="btn btn-webamooz_net">ایجاد دوره</button>
+            </form>
+        </div>
     </div>
-</div>
 @endsection
 
 @section('js')
-<script src="/panel/js/tagsInput.js"></script>
+    <script src="/panel/js/tagsInput.js?v=12"></script>
 @endsection
